4d4c149e9d78c0ea620d9146b9f3a0d5952720a9
Csanad Kolcsei <csanad_kolcsei@epam.com>
2021-04-19 16:52:32 +0200

    Removed extra classes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 @Module({
-    imports: [MongooseModule.forRoot(process.env.MONGO_URL)],
-    controllers: [AppController],
-    providers: [AppService]
+    imports: [MongooseModule.forRoot(process.env.MONGO_URL)]
 })
 export class AppModule {}
