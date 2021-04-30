3ecf466fc782a0187827cd2e8493f4c2d488d737
Csanad Kolcsei <csanad_kolcsei@epam.com>
2021-04-30 13:15:10 +0200

    Added task create and task delete gateways

--- src/task/task.gateway.ts
+++ src/task/task.gateway.ts
@@ -3,13 +3,31 @@ import { Socket } from 'socket.io';
 import * as config from 'config';
 import { Logger } from '@nestjs/common';
 
-const socketGateway = config.get('server').socketGateway;
+const socketConfig = config.get('socket');
 
 @WebSocketGateway()
 export class TaskGateway implements OnGatewayInit {
-    @SubscribeMessage(socketGateway)
-    handleMessage(client: Socket, payload: string): void {
-        client.broadcast.emit(socketGateway, payload);
+    @SubscribeMessage(socketConfig.update)
+    handleUpdateMessage(client: Socket, payload: string): void {
+        this.logActivity(socketConfig.update);
+        client.broadcast.emit(socketConfig.update, payload);
+    }
+
+    @SubscribeMessage(socketConfig.delete)
+    handleDeleteMessage(client: Socket, payload: string): void {
+        this.logActivity(socketConfig.delete);
+        client.emit(socketConfig.delete, payload);
+        client.broadcast.emit(socketConfig.delete, payload);
+    }
+
+    @SubscribeMessage(socketConfig.add)
+    handleAddMessage(client: Socket, payload: string): void {
+        this.logActivity(socketConfig.add);
+        client.broadcast.emit(socketConfig.add, payload);
+    }
+
+    private logActivity(messageType: string) {
+        Logger.log(`Forwarding ${messageType} message`, this.constructor.name);
     }
 
     afterInit() {
